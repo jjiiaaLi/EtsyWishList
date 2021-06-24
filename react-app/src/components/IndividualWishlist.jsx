7ff43252b4376e5282893bb83c0c52fc1f9cc913
james <jodm522@gmail.com>
2021-06-24 14:38:58 -0500

    style wishlist page

--- react-app/src/components/IndividualWishlist.jsx
+++ react-app/src/components/IndividualWishlist.jsx
@@ -1,69 +1,64 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {useParams, useHistory, Link} from 'react-router-dom';
-import {getWishlistItems,removeProductFromWishlist, clearProducts} from '../store/product';
-import './IndividualWishlist.css';
+import { useParams, useHistory, Link } from "react-router-dom";
+import {
+  getWishlistItems,
+  removeProductFromWishlist,
+  clearProducts,
+} from "../store/product";
+import "./IndividualWishlist.css";
 
-export default function IndividualWishlist(){
-    const {wishlistId}=useParams()
-    const dispatch=useDispatch()
-    const products=useSelector(state=>Object.values(state.product))
+export default function IndividualWishlist() {
+  const { wishlistId } = useParams();
+  const dispatch = useDispatch();
+  const products = useSelector((state) => Object.values(state.product));
 
+  useEffect(() => {
+    dispatch(getWishlistItems(wishlistId));
+    return () => dispatch(clearProducts());
+  }, [dispatch]);
 
-    useEffect(()=>{
-        dispatch(getWishlistItems(wishlistId))
-        return ()=>dispatch(clearProducts())
-    },[dispatch])
+  const removeItem = async (e) => {
+    e.preventDefault();
+    await dispatch(removeProductFromWishlist(e.target.value));
+    window.location.reload();
+  };
 
-    const removeItem=async(e)=>{
-        e.preventDefault()
-        await dispatch(removeProductFromWishlist(e.target.value))
-        window.location.reload()
-    }
-
-    const openInNewTab=(e)=>{
-        e.preventDefault()
-        const newWindow = window.open(`http://etsy.com/listing/${e.target.value}`, '_blank', 'noopener,noreferrer')
-        if (newWindow) newWindow.opener = null
-    }
-
-    return (
-      <div className="wishlistContainer">
-        {products.map((product) => (
-          <div className="productCard">
-            <div className="productTitle">{product.name.slice(0, 20)}</div>
-            <div className="ProductInfo">
-             <Link
-              className="productLink"
-              to={`/products/${product.product_id}`}
-             >
-              <div
-                className="productImage"
-                style={{ backgroundImage: `url("${product.image_url}")` }}
-              ></div>
-             </Link>
-
-                <div>${product.price}</div>
-            </div>
-
-            <div className="productButtonContainer">
-              <button
-                className="button"
-                value={product.id}
-                onClick={removeItem}
-              >
-                delete
-              </button>
-              <button
-                className="button"
-                value={product.product_id}
-                onClick={openInNewTab}
-              >
-                View in Etsy
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
+  const openInNewTab = (e) => {
+    e.preventDefault();
+    const newWindow = window.open(
+      `http://etsy.com/listing/${e.target.value}`,
+      "_blank",
+      "noopener,noreferrer"
     );
+    if (newWindow) newWindow.opener = null;
+  };
+
+  return (
+    <div className="wishlistContainer">
+      {products.map((product) => (
+        <div className="productCardWL" style={{ backgroundImage: `url("${product.image_url}")` }}>
+          <div className="productTitle">{product.name.slice(0, 20)}...</div>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+            <div className="productPrice">${product.price}</div>
+          <div className="productButtonContainer">
+            <button className="button" value={product.id} onClick={removeItem}>
+              Remove
+            </button>
+            <button
+              className="button"
+              value={product.product_id}
+              onClick={openInNewTab}
+            >
+              View in Etsy
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
--- react-app/src/components/Products.js
+++ react-app/src/components/Products.js
@@ -23,8 +23,8 @@ export default function Products(){
         {products.map((product, i) => (
 
           <Link key={product.listing_id} className='productCard'  onMouseEnter={()=> {setFocusTitle(product.listing_id)}} onMouseLeave={()=>setFocusTitle(null)} to={`/products/${product.listing_id}`}>
-            {product.title && <div className="title">{`${product.title.slice(0,((focusTitle === product.listing_id) ? 50 : 30))}...`}</div>}
             {product.MainImage && <div className="listingImage"style={{backgroundImage: `url("${product.MainImage.url_fullxfull}")`,}}></div>}
+            {product.title && <div className="title">{`${product.title.slice(0,((focusTitle === product.listing_id) ? 50 : 30))}...`}</div>}
           <div className="productInfo">
             <div className="prices">{"$" + product.price}</div>
             <div className="seeMore">Click to see more</div>
